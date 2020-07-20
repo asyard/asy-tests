d73d0b8cee5132ad7584d9806357a952cbf90d68
asyard <asyard@gmail.com>
2020-07-20 16:32:08 +0300

    asy-tests : httpurlconnection - disconnect after operation

--- asy-test-httpurlconnection/src/main/java/com/asy/test/httpurlconnection/HttpTest.java
+++ asy-test-httpurlconnection/src/main/java/com/asy/test/httpurlconnection/HttpTest.java
@@ -2,6 +2,7 @@ package com.asy.test.httpurlconnection;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -12,14 +13,23 @@ public class HttpTest {
         if (args != null && args.length > 0 && !args[0].isEmpty()) {
             connectionUrl = args[0];
         }
-        URL url = new URL(connectionUrl);
-        URLConnection connection = url.openConnection();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String inputLine, inputLines = "";
-        while ((inputLine = bufferedReader.readLine()) != null) {
-            inputLines += inputLine;
+        HttpURLConnection connection = null;
+        try {
+            URL url = new URL(connectionUrl);
+            connection = (HttpURLConnection) url.openConnection();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine, inputLines = "";
+            while ((inputLine = bufferedReader.readLine()) != null) {
+                inputLines += inputLine;
+            }
+            System.out.println(inputLines);
+            bufferedReader.close();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
         }
-        System.out.println(inputLines);
-        bufferedReader.close();
+
+
     }
 }
