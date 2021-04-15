e5bb6e4268a614efac447c954c69c1609034c3de
asyard <asyard@gmail.com>
2021-04-15 21:12:28 +0300

    asy-tests : java8 to latest goes on

--- asy-test-java8tolatest/src/main/java/com/asy/test/lambda/FunctnlInterface1.java
+++ asy-test-java8tolatest/src/main/java/com/asy/test/lambda/FunctnlInterface1.java
@@ -5,16 +5,24 @@ package com.asy.test.lambda;
 // with functional interfaces to avoid addition of extra methods accidentally
 @FunctionalInterface
 public interface FunctnlInterface1 {
-    void test();
+    /*public*/ void test();
     //void test2(); //: compilation error if more than 1 abstract method exists
 
-    default void testDefault1() {
+    // with java8, we can define default methods in interfaces
+    // Default methods allow us to add new methods to an interface that are automatically
+    // available in the implementations. Thus, there's no need to modify the implementing classes.
+    /*public*/ default void testDefault1() {
         System.out.println("default method 1");
         test();
     }
 
+    // more than 1 default method can exist
     default void testDefault2() {
         System.out.println("default method 2");
     }
+
+    /*private default void testDefault3() {
+        // compile error. Illegal combination of modifiers: 'default' and 'private'
+    }*/
 
 }
--- asy-test-java8tolatest/src/main/java/com/asy/test/lambda/FunctnlInterface2.java
+++ asy-test-java8tolatest/src/main/java/com/asy/test/lambda/FunctnlInterface2.java
@@ -2,12 +2,25 @@ package com.asy.test.lambda;
 
 @FunctionalInterface
 public interface FunctnlInterface2 {
+
     String test2(String param1, int param2);
 
-
-    // after java8, we can define static and default methods in interfaces
-    // more than 1 static/default methods can exist
-    static void staticMethod1() {
+    // with java8, we can define static methods in interfaces
+    // The idea behind static interface methods is to provide a simple mechanism that
+    // allows us to increase the degree of cohesion of a design by putting together
+    // related methods in one single place without having to create an object.
+    // Pretty much the same can be done with abstract classes. The main difference
+    // lies in the fact that abstract classes can have constructors, state, and behavior.
+    /*public*/ static void staticMethod1() {
         System.out.println("inferface2 static method");
+    }
+
+    // more than 1 static method can exist
+    static void staticMethod2() {
+        System.out.println("inferface2 static method 2");
+    }
+
+    private static void staticMethod3() {
+        System.out.println("inferface2 static method 3 - private");
     }
 }
